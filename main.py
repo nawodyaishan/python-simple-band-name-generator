449f5eee937f389e4d099627d45e085e443ade9a
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2021-11-26 23:57:02 +0530

    Update main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+print("__________________________________")
+print("Welcome to the Band Name Generator")
+print("__________________________________")
+
+city = input("\nPlease state the city you grew up in ? ")
+
+print("\nYour Birth City = >> " + city)
+
+pet = input("\nPlease state your pet ? ")
+
+print("\nYour Birth City = >> " + pet)
+
+print("\nHere Accept Your Band Name >> " + city + " " + pet + "s")
